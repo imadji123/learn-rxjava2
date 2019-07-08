70b6df3106de7d3f0b0d93c39252cd7559eba199
Adji Zulfiqar Fauzi <adji@hooq.tv>
2019-07-08 09:52:50 +0700

    Initial project

---
+++ app/src/main/java/com/imadji/learn/rxjava2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.imadji.learn.rxjava2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
